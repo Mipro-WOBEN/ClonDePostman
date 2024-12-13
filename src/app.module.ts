ae87a9511772e00adcdb585267801e4845304221
Bryan Darien Cordova Pujay <bryancordovapujay@gmail.com>
2024-12-13 08:54:41 -0500

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,23 +8,31 @@ import { ColeccionesModule } from './colecciones/colecciones.module';
 import { MethodsModule } from './methods/methods.module';
 import { ProyectosModule } from './proyectos/proyectos.module';
 import { RolesModule } from './roles/roles.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'sa',
-      password: '',
-      database: 'postman',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      autoLoadEntities: true,
-      extra: {
-        encrypt: true, // Habilitar SSL
-        trustServerCertificate: true, // Deshabilitar la verificación del certificado
-      },
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT'), 10),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        extra: {
+          encrypt: true, // Habilitar SSL
+          trustServerCertificate: true, // Deshabilitar la verificación del certificado
+        },
+      }),
     }),
     AuthModule,
     UsersModule,
